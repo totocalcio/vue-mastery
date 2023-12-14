e2aa15d4678bddc13ea9899ca14b676afabbfe76
totocalcio <totohyper@yahoo.co.jp>
2023-12-14 15:15:10 +0900

    Add TypeScript and Vue files for a friendly Vue 3 project

---
+++ typescript-friendly-vue3/src/components/Counter.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import fetchCount from "../fetchCount";
+import ControlBar from "./ControlBar.vue";
+
+interface Props {
+  limit: number;
+  alertMessageOnLimit?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  alertMessageOnLimit: "can not go any higher",
+});
+
+const count = ref<number | null>(null);
+
+onMounted(() => {
+  fetchCount((initialCount) => {
+    count.value = initialCount;
+  });
+});
+
+function addCount(num: number) {
+  if (count.value !== null) {
+    if (count.value >= props.limit) {
+      alert(props.alertMessageOnLimit);
+    } else {
+      count.value += num;
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>{{ count }}</p>
+    <ControlBar @add-count="addCount" @reset-count="count = 0"></ControlBar>
+  </div>
+</template>
